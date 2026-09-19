f02df84eee59154f12b3028d81d6fe21a53ab593
Kshitiz Gupta <kshitizgupta.gmgr@gmail.com>
2026-09-20 03:22:41 +0530

    Fixed CORS origin

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencart-client-b32qoxrov-kshitizguptagmgr-1745.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-client-eosin.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
