28b1a0bb6a2a53edda249e14c10e50e31b673fe4
Kshitiz Gupta <kshitizgupta.gmgr@gmail.com>
2026-09-20 03:00:03 +0530

    Fix CORS origin and backend URL

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'greencart-client-eosin.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-backend-jwbh.onrender.com']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
